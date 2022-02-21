6951c2c34d19ed5a241b00512d19f6e709b2dd1f
Eduardo Medrano <eduardomedranop@gmail.com>
2022-02-20 21:21:51 -0300

    Fix URL

--- js/main.js
+++ js/main.js
@@ -20,23 +20,61 @@ function desafio_1() {
       break;
 
     case "B":
-      alert("Hola esto es un app contador, hasta donde quieres contar")
+      alert("Hola esto es un app contador, hasta donde quieres contar");
       let contador = 1;
-      let numero_hasta = prompt("Hasta que numero");
-      let resultado_contador = '';
-      let resultado_final = '';
-    do {
-      resultado_contador = contador++ + "\n";
-      resultado_final += resultado_contador;
-  
-    } while (contador <= numero_hasta);
- alert(resultado_final);
- console.log(resultado_final);
+      let numero_hasta = parseInt(prompt("Hasta que numero"));
+      let resultado_contador = "";
+      let resultado_final = "";
+      do {
+        resultado_contador = contador++ + "\n";
+        resultado_final += resultado_contador;
+      } while (contador <= numero_hasta);
+      alert(resultado_final);
+      console.log(resultado_final);
       break;
 
     default:
       alert("Debe Elejir una opcion Valida!");
       break;
   }
+}
 
+function simulador_de_credito() {
+  //  Variable de Tipo de Creditos
+  let credito_tipo1 = "Vehiculo";
+  let credito_tipo2 = "Casa";
+  let credito_tipo3 = "Credito de Consumo";
+
+  // Vehiculos
+  let carros = ["Ford Fusion", "Nissan Versa", "Chevrolet Onix"];
+  let nombreCarros = "";
+
+  const opcion_credito = parseInt(prompt("Atención Elejir una de la Opciones del tipo de Credito: \n1 : " + credito_tipo1 + "\n2 : " + credito_tipo2 + "\n3 : " + credito_tipo3));
+
+  switch (opcion_credito) {
+    case 1:
+      alert("Bienvenido al Simulador de Credito " + credito_tipo1);
+        for (i = 0; i < carros.length; i++) {
+          temp = carros[i] + "\n";
+          nombreCarros += temp;
+        };
+      prompt(`Cual Carro deseas: \n${nombreCarros} `)
+
+      break;
+
+    case 2:
+      alert("Bienvenido al Simulador de Credito " + credito_tipo2);
+      alert("Este Momento Simulador de Credito " + credito_tipo2 + "no esta disponible disculpa molestia ocasionada.");
+      break;
+
+    case 3:
+      alert("Bienvenido al Simulador de Credito " + credito_tipo3);
+      alert("Este Momento Simulador de Credito " + credito_tipo2 + "no esta disponible disculpa molestia ocasionada.");
+      break;
+
+    default: {
+      alert("Debe Elejir una de la opciones Valida");
+      return simulador_de_credito;
+    }
+  }
 }
