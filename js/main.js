e203b2dde12c4d0399e43ae4cba271842bf7f72f
Eduardo Medrano <eduardomedranop@gmail.com>
2022-03-06 20:02:21 -0300

    Desafio de Pre Entrega 1

--- js/main.js
+++ js/main.js
@@ -135,41 +135,113 @@ function simulador_de_credito() {
 // Desafio 3
 
 function metodos_array() {
-    alert("Todo sale en la consola")
+  alert("Todo sale en la consola");
 
   const personajes = ["goku", "vegeta", "gohan", "trunk", "broly", "frezzer"];
 
-  numero = [1,2,3,4,5]
- 
+  numero = [1, 2, 3, 4, 5];
+
   personajes.push("krilin"); // Agregar al Final del Array
   console.log(`Agregado Krilin con Push  ${personajes}`);
- 
+
   personajes.pop(); // Eliminar al Final del Array
   console.log(`Eliminar Krilin con pop  ${personajes}`);
-  
+
   personajes.shift(); // Eliminar al Principio del Array
   console.log(`Eliminar al principio del array con shift del array ${personajes}`);
- 
+
   personajes.unshift("goku"); // Agrega al Principio del Array
   console.log(`Agrega al principio del array con unshift del array ${personajes}`);
 
   personajes.concat("Picolo"); // Agrega al Final  concadenado del Array
   console.log(`Agrega al Final concadenado del array con concat del array ${personajes}`);
 
-  console.log("Con JOIN " + personajes.join('---------'));
-  
+  console.log("Con JOIN " + personajes.join("---------"));
+
   conteo = personajes.length;
 
   console.log("Cuanto personajes con length " + conteo);
 
-
   console.log("Metodo slice " + personajes.slice(2));
 
-   console.log("Metodo IndexOf Buscador por array ubicado en " + personajes.indexOf('trunk'));
+  console.log("Metodo IndexOf Buscador por array ubicado en " + personajes.indexOf("trunk"));
 
   console.log("Metodo includes Buscador por array si trunk existe?  " + personajes.includes("trunk"));
 
   console.log("Metodo reverse todo ordena todo arrevez los array " + personajes.reverse);
-  
-
 }
+
+
+
+// Desafio Pre-Entrega
+
+
+
+function calculo_credito_carro(montocredito, coutas) {
+  let interes = 3 / 100;
+  let resultado = (montocredito * (interes * Math.pow(1 + interes, coutas))) / (Math.pow(1 + interes, coutas) - 1);
+  return `${resultado.toFixed(0)} en ${coutas} Coutas`;
+}
+
+// Vehiculos
+let carros = [
+  {
+    modelo: "Ford Fusion",
+    valor: 50000000,
+  },
+  {
+    modelo: "Nissan Versa",
+    valor: 20000000,
+  },
+  {
+    modelo: "Chevrolet Onix",
+    valor: 13000000,
+  },
+];
+let nombreCarros = "";
+
+function simulador_de_credito_pe1() {
+  //  Variable de Tipo de Creditos
+  let credito_tipo1 = "Vehiculo";
+  let credito_tipo2 = "Casa";
+  let credito_tipo3 = "Credito de Consumo";
+
+  let nombreCarros = "";
+
+  const opcion_credito = parseInt(prompt("Atención Elejir una de la Opciones del tipo de Credito: \n1 : " + credito_tipo1 + "\n2 : " + credito_tipo2 + "\n3 : " + credito_tipo3));
+
+  switch (opcion_credito) {
+    case 1:
+      alert("Bienvenido al Simulador de Credito " + credito_tipo1);
+
+      carros.forEach(function (valor, index) {
+        temp = `${index + 1}- ${valor.modelo} El Precio Es: ${valor.valor} \n`;
+        nombreCarros += temp;
+      });
+      let elecion = parseInt(prompt(`Cual Carro deseas: \n${nombreCarros} `));
+      if (elecion == 1 || elecion == 2 || elecion == 3) {
+        let coutas = parseInt(prompt(`Cuanta Coutas deseas?`));
+        let vehiculo_valor = carros[elecion-1].valor;
+        alert(calculo_credito_carro(vehiculo_valor, coutas));
+      } else {
+        alert("La Opcion no es Valida");
+      }
+
+      break;
+    case 2:
+      alert("Bienvenido al Simulador de Credito " + credito_tipo2);
+      alert("Este Momento Simulador de Credito " + credito_tipo2 + "no esta disponible disculpa molestia ocasionada.");
+      break;
+
+    case 3:
+      alert("Bienvenido al Simulador de Credito " + credito_tipo3);
+      alert("Este Momento Simulador de Credito " + credito_tipo2 + "no esta disponible disculpa molestia ocasionada.");
+      break;
+
+    default: {
+      alert("Debe Elejir una de la opciones Valida");
+      return simulador_de_credito;
+    }
+  }
+}
+// END Desafio Pre-Entrega
