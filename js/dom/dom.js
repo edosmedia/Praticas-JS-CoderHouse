d96fe08f2949741b34d4d8ebd1f9e3535c880742
Eduardo Medrano <eduardomedranop@gmail.com>
2022-03-07 23:38:45 -0300

    parte2 de desafio complementario DOM

--- js/dom/dom.js
+++ js/dom/dom.js
@@ -1,3 +1,72 @@
+// const personajes = [
+//   {
+//     nombre: "Goku",
+//     transformaciones: 10,
+//     raza: "Sayayin",
+//   },
+//   {
+//     nombre: "Vegeta",
+//     transformaciones: 10,
+//     raza: "Sayayin",
+//   },
+//   {
+//     nombre: "Gohan",
+//     transformaciones: 10,
+//     raza: "Humano",
+//   },
+// ];
+
+// const tabladepersonaje = document.getElementById("listaPersonaje");
+
+// let cuerpoTabla = document.createElement("tbody");
+
+// function agregarpersonaje() {
+//       let nombre_personaje = prompt("Cual nombre del personaje");
+//       let transformaciones_personaje = prompt("Cuantas transformaciones");
+//       let nivel_personaje = prompt("Cuantas transformaciones");
+//       personajeExtra = {
+//         nombre: nombre_personaje,
+//         transformaciones: transformaciones_personaje,
+//         raza: nivel_personaje,
+//       };
+//       let celdas = "";
+//       for (let dato in personajeExtra){
+//         celdas = celdas + '<td>'+personajeExtra[dato]+'</td>';
+//       }
+//       document.getElementById("listaPersonaje").insertRow(-1).innerHTML = celdas;
+
+      
+// };
+
+
+//   function agregarpersonaje() {
+//       let nombre_personaje = prompt("Cual nombre del personaje");
+//       let transformaciones_personaje = prompt("Cuantas transformaciones");
+//       let nivel_personaje = prompt("Cuantas transformaciones");
+//       let celdas = + '<td>'+nombre_personaje+'</td>'+ '<td>'+transformaciones_personaje+'</td>'+ '<td>'+nivel_personaje+'</td>';
+//       document.getElementById("listaPersonaje").insertRow(-1).innerHTML = celdas;
+//   };
+
+// personajes.forEach((p) => {
+//   let fila = document.createElement("tr");
+
+//   td = document.createElement("td");
+//   td.innerText = p.nombre;
+//   fila.append(td);
+
+//   td = document.createElement("td");
+//   td.innerText = p.transformaciones;
+//   fila.append(td);
+
+//   td = document.createElement("td");
+//   td.innerText = p.raza;
+//   fila.append(td);
+
+//   cuerpoTabla.append(fila);
+// });
+
+// tabladepersonaje.append(cuerpoTabla);
+
 const personajes = [
   {
     nombre: "Goku",
@@ -19,34 +88,70 @@ const personajes = [
 const tabladepersonaje = document.getElementById("listaPersonaje");
 
 let cuerpoTabla = document.createElement("tbody");
-
-
-function agregarpersonaje() {
-    class personaje {
-    constructor(nombre, transformaciones, nivel) {
+  
+class personaje {
+    constructor(nombre, transformaciones, raza) {
       this.nombre = nombre;
       this.transformaciones = transformaciones;
-      this.nivel = nivel;
-    }    
-  }  
-      const nombre_personaje = prompt("Cual nombre del personaje");
-      const transformaciones_personaje = prompt("Cuantas transformaciones");
-      const nivel_personaje = prompt("Cuantas transformaciones");
+      this.raza = raza;
+    }
+  }
+
+function agregarpersonaje() {
+  const nombre_personaje = prompt("Cual nombre del personaje");
+  const transformaciones_personaje = prompt("Cuantas transformaciones");
+  const nivel_personaje = prompt("Cuantas transformaciones");
   let nombre = nombre_personaje;
   let transformaciones = transformaciones_personaje;
   let nivel = nivel_personaje;
   // console.log(nombre);
-  nuevopersonajes = new personaje("Picolo", 4, "Namekiano" );
-  console.log(nuevopersonajes);
-  agregar();
-};
+  nuevopersonajes = new personaje (nombre, transformaciones, nivel);
+  // console.log(nuevopersonajes);
 
-function agregar () {
-  var resultado = personajes.push(nuevopersonajes);
+    personajes.push(nuevopersonajes);
+    let fila = document.createElement("tr");
+
+    td = document.createElement("td");
+    td.innerText = nuevopersonajes.nombre;
+    fila.append(td);
+
+    td = document.createElement("td");
+    td.innerText = nuevopersonajes.transformaciones;
+    fila.append(td);
+
+    td = document.createElement("td");
+    td.innerText = nuevopersonajes.raza;
+    fila.append(td);
+
+    cuerpoTabla.append(fila);
+}
+
+
+function eliminar() {
+  let el = document.getElementsByTagName("tr");
   
-};
 
-personajes.forEach((p) => {
+}
+
+// function agregar(nuevopersonajes) {
+//   personajes.push(nuevopersonajes);
+//   let fila = document.createElement("tr");
+
+//   td = document.createElement("td");
+//   td.innerText = nuevopersonajes.nombre;
+//   fila.append(td);
+
+//   td = document.createElement("td");
+//   td.innerText = nuevopersonajes.transformaciones;
+//   fila.append(td);
+
+//   td = document.createElement("td");
+//   td.innerText = nuevopersonajes.raza;
+//   fila.append(td);
+
+//   cuerpoTabla.append(fila);
+// }
+const listaPersonaje = () => personajes.forEach((p) => {
   let fila = document.createElement("tr");
 
   td = document.createElement("td");
@@ -65,3 +170,4 @@ personajes.forEach((p) => {
 });
 
 tabladepersonaje.append(cuerpoTabla);
+listaPersonaje();
